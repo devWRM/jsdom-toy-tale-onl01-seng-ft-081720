e606fefedef8ff0bac6f55f77f939edd9caa9796
devWRM <mcwen@comcast.net>
2020-11-22 21:43:08 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let addToy = false;
+let addToyForm = document.querySelector(".add-toy-form")
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -19,36 +20,37 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 
-// FETCH resource:  https://learn.co/tracks/online-software-engineering-structured/front-end-web-programming/communication-with-the-server/use-fetch
 
-// The getAllToys function is a GET request
+
+  // FETCH resource:  https://learn.co/tracks/online-software-engineering-structured/front-end-web-programming/communication-with-the-server/use-fetch
+
+  // The getAllToys function is a GET request
   function getAllToys() {
-    // console.log("This is the get all toys function.")   //  <= call function to test this code which confirms the function works
+      // console.log("This is the get all toys function.")   //  <= call function to test this code which confirms the function works
 
+      // Make a 'GET' request to fetch all the toy objects
+      // ?? Where ever getAllToys() is called will get a json object ??
+      //  - Reference Dog CEO Challenge
+      return fetch("http://localhost:3000/toys")
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(json){
 
-// Make a 'GET' request to fetch all the toy objects
-// Where ever getAllToys() is called will get a json object
-//  - Reference Dog CEO Challenge
-    return fetch("http://localhost:3000/toys")
-      .then(function(response) {
-        return response.json();
+          // console.log(json)   //  <= test to see if json object was retrieved
+          // console.log(json[0])   //  <= test to see if json object was retrieved
+          // console.log(json[0]["id"])   //  <= test to see if json object was retrieved
+          // console.log(json[0]["name"])   //  <= test to see if json object was retrieved
+          // console.log(json[0]["image"])   //  <= test to see if json object was retrieved
+          // console.log(json[0]["likes"])   //  <= test to see if json object was retrieved
+          
+          // return json
+          buildBrowserToyCollection(json)
       })
-      .then(function(json){
 
-        // console.log(json)   //  <= test to see if json object was retrieved
-        // console.log(json[0])   //  <= test to see if json object was retrieved
-        // console.log(json[0]["id"])   //  <= test to see if json object was retrieved
-        console.log(json[0]["name"])   //  <= test to see if json object was retrieved
-        // console.log(json[0]["image"])   //  <= test to see if json object was retrieved
-        // console.log(json[0]["likes"])   //  <= test to see if json object was retrieved
-        
-        // return json
-        buildBrowserToyCollection(json)
-    })
-
-// Fetch gets the data from the URL server address and returns it as a response
-// The fetched response is passed into a function that converts the response to usable JSON
-// The JSON is passed into a function that can now manipulate the DOM in the browser
+      // Fetch gets the data from the URL server address and returns it as a response
+      // The fetched response is passed into a function that converts the response to usable JSON
+      // The JSON is passed into a function that can now manipulate the DOM in the browser
 
   }
 
@@ -58,50 +60,100 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-function buildBrowserToyCollection(jsonToysList) {
+  function buildBrowserToyCollection(jsonToysList) {
 
 // debugger;
 
-  // console.log(jsonToysList)   //  <= test to make sure the json object (jsonToysList) was passed in
-  // console.log(jsonToysList[0]["name"])  //  <= test to make sure the json object (jsonToysList) was passed in
+    // console.log(jsonToysList)   //  <= test to make sure the json object (jsonToysList) was passed in
+    // console.log(jsonToysList[0]["name"])  //  <= test to make sure the json object (jsonToysList) was passed in
 
-
-    
     let toyCollection = document.getElementById("toy-collection")
 
-  // Appending Reference:  https://learn.co/tracks/online-software-engineering-structured/front-end-web-programming/manipulating-the-dom/removing-altering-and-inserting-html-lab 
-  // Can use for...of OR forEach
-  for (const toy of jsonToysList) {
-    let card = document.createElement("div")
-    card.innerHTML = `<h2>${toy["name"]}</h2><img src= "${toy["image"]}" class = "toy-avatar" alt = "${toy["name"]}-image"><p>LIKES = ${toy["likes"]}</p><button class = "like-btn" data-anyname = "like">like <3</button>`
+    // Appending Reference:  https://learn.co/tracks/online-software-engineering-structured/front-end-web-programming/manipulating-the-dom/removing-altering-and-inserting-html-lab 
+    // Can use for...of OR forEach
+    for (const toy of jsonToysList) {
+      let card = document.createElement("div")
+      card.innerHTML = `<h2>${toy["name"]}</h2><img src= "${toy["image"]}" class = "toy-avatar" alt = "${toy["name"]}-image"><p>LIKES = ${toy["likes"]}</p><button class = "like-btn" data-anyname = "like">like <3</button>`
+
+      // card.innerText = toy["id"] + ". " + toy["name"]  <= testing
+      toyCollection.appendChild(card)
+
+    }
 
 
-    // card.innerText = toy["id"] + ". " + toy["name"]  <= testing
-    toyCollection.appendChild(card)
+    // TEST:  Make a <div class="card"> for each toy and add it to the toy-collection div
+        // let card = document.createElement("div")
+        // card.innerText = "I'm alive!!!"
+        // let toyCollection = document.getElementById("toy-collection")
+        // toyCollection.appendChild(card)
+
+  }
 
 
 
-// IMG example:   <img src="https://media.giphy.com/media/3o6MbkZSYy4mI3gLYc/giphy.gif" alt="A policeman">
+
+  // Submit EXAMPLE Task Lister Lite
+  // Post request EXAMPLE: https://learn.co/tracks/online-software-engineering-structured/front-end-web-programming/communication-with-the-server/sending-data-with-fetch-lab
+  addToyForm.addEventListener("submit", function(e) {
+    // Use e.preventDefault() to stop the automatic handling of submit so I can use JavaScript to handle submit
+    e.preventDefault()
+
+
+    // console.log(e)          //  <= CLICK SUBMIT to test if correct element listened to (returns submit event)
+    // console.log(e.target)   //  <= CLICK SUBMIT to test if correct element listened to (returns the add-toy-form Form Tag & contents)
+
+
+    // ======= START Post
+
+    addAToy(e.target)
+
+    // ======= END Post
+
+    addToyForm.reset()
+
+  })
+
+
+  
+  function addAToy(userToyInput) {
+
+// debugger;
+
+    console.log("You made it again!!!")   // <=  CLICK SUBMIT to test that I made it to this function
+    console.log(userToyInput)
+    // console.log(`${userToyInput}`)
+
+
+    // 
+    //   return fetch( 'http://localhost:3000/toys', {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //         "Accept": "application/json"
+    //       },
+    //       body: JSON.stringify( {
+    //         name,
+    //         email
+    //       } )
+    //     } )
+    //     .then( function ( response ) {
+    //       return response.json()
+    //     } )
+    //     .then( function ( object ) {
+    //       document.body.innerHTML = object[ "id" ]
+    //     } )
+    //     .catch( function ( error ) {
+    //       document.body.innerHTML = error.message
+    //     } )
+    // 
+
+
 
 
   }
 
 
-//   for (const element of array) {
-//     console.log(element);
-// }
 
-
-
-// TEST:  Make a <div class="card"> for each toy and add it to the toy-collection div
-    // let card = document.createElement("div")
-    // card.innerText = "I'm alive!!!"
-    // let toyCollection = document.getElementById("toy-collection")
-    // toyCollection.appendChild(card)
-
-}
-
-  // buildBrowserToyCollection(getAllToys)   //  <= function call to test function
 
 
 
@@ -109,6 +161,8 @@ function buildBrowserToyCollection(jsonToysList) {
 
 
 });
+
+
 
 
 // document.addEventListener('DOMContentLoaded', function() {
